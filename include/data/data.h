7f9f5c2f2740b4d459380fa90f6826778739fa34
yungen-lu <70182238+yungen-lu@users.noreply.github.com>
2021-07-05 16:48:21 +0800

    add new wrapper structs

--- include/data/data.h
+++ include/data/data.h
@@ -1,3 +1,5 @@
+#ifndef DATA_H
+#define DATA_H
 #include <../sds/sds.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -10,8 +12,15 @@ typedef struct {
     sds value;
     bool exist;
 } hashStruct;
+typedef struct {
+    hashStruct *hashTable;
+    size_t hashTableSize;
+} InMemStructs;
+
 hashStruct *createHashTable(size_t hashTableSize);
+InMemStructs *createInMemStructs(const size_t hashTableSize);
 void insertDataToTable(hashStruct *hashTable, const size_t hashTableSize, const sds key, const sds value);
 sds findByKeyInTable(hashStruct *hashTable, const size_t hashTableSize, const sds key);
 bool deleteByKeyInTable(hashStruct *hashTable, const size_t hashTableSize, const sds key);
 void deleteHashTable(hashStruct *hashTable, const size_t hashTableSize);
+#endif
--- src/data/data.c
+++ src/data/data.c
@@ -1,5 +1,7 @@
 #include "data.h"
 
+#include "sds.h"
+
 hashStruct *createHashTable(const size_t hashTableSize) {
     hashStruct *new = (hashStruct *)malloc(sizeof(hashStruct) * hashTableSize);
     for (size_t i = 0; i < hashTableSize; i++) {
@@ -16,6 +18,12 @@ void deleteHashTable(hashStruct *hashTable, const size_t hashTableSize) {
     }
     free(hashTable);
 }
+InMemStructs *createInMemStructs(const size_t hashTableSize) {
+    InMemStructs *new = (InMemStructs *)malloc(sizeof(InMemStructs));
+    new->hashTable = createHashTable(hashTableSize);
+    new->hashTableSize = hashTableSize;
+    return new;
+}
 static const int probeFunc(const int i) { return i * i * (i % 2) * -1; }
 static size_t getTablePos(const size_t hashTableSize, const sds string) {
     return murmurhash(string, (uint32_t)sdslen(string), SEED) % hashTableSize;
@@ -24,7 +32,7 @@ void insertDataToTable(hashStruct *hashTable, const size_t hashTableSize, const 
     const size_t pos = getTablePos(hashTableSize, key);
     for (size_t i = 0; i < hashTableSize; i++) {
         const size_t tmp = pos + probeFunc(i);
-        if (hashTable[tmp].exist == false) {
+        if ((hashTable[tmp].exist == false || sdscmp(hashTable[tmp].key, key) == 0)) {
             hashTable[tmp].key = sdsdup(key);
             hashTable[tmp].value = sdsdup(value);
             hashTable[tmp].exist = true;
@@ -38,7 +46,7 @@ void insertDataToTable(hashStruct *hashTable, const size_t hashTableSize, const 
 sds findByKeyInTable(hashStruct *hashTable, const size_t hashTableSize, const sds key) {
     const size_t pos = getTablePos(hashTableSize, key);
     for (size_t i = 0; i < hashTableSize; i++) {
-        const size_t tmp = pos + probeFunc(i);
+        const size_t tmp = (pos + probeFunc(i)) % hashTableSize;
         if (hashTable[tmp].exist == true && (sdscmp(hashTable[tmp].key, key) == 0)) {
             return hashTable[tmp].value;
         }
