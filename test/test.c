69eaeb46567be6d094a1a936361f827da86202d5
yungen-lu <70182238+yungen-lu@users.noreply.github.com>
2021-07-07 17:07:36 +0800

    add test files

---
+++ test/test.c
@@ -0,0 +1,27 @@
+#include "parseInput.h"
+#include "unity.h"
+#include "unity_internals.h"
+void setUp(void) {}
+
+void tearDown(void) {
+    // clean stuff up here
+}
+void test_CmdParse(void) {
+    char *arrOfCmd[] = {"DEL",   "DUMP", "EXISTS", "RENAME", "SET",   "GET",
+                        "LPUSH", "LPOP", "LINDEX", "LLEN",   "RPUSH", "RPOP"};
+    char *arrOfWrongCmd[] = {"qwe", "asd", "EOIW", "askd", "WS"," ","\n","\t"};
+    size_t sizeOfarrOfCmd = sizeof(arrOfCmd) / sizeof(arrOfCmd[0]);
+    size_t sizeOfarrOfWrongCmd = sizeof(arrOfWrongCmd) / sizeof(arrOfWrongCmd[0]);
+    // TEST_ASSERT_EQUAL_INT(DEL_CMD,getCommand("DEL"));
+    for (size_t i = 0; i < sizeOfarrOfCmd; i++) {
+        TEST_ASSERT_EQUAL_INT(DEL_CMD + i, getCommand(arrOfCmd[i]));
+    }
+    for (size_t i = 0; i < sizeOfarrOfWrongCmd; i++) {
+        TEST_ASSERT_EQUAL_INT(CMD_NOT_FOUND, getCommand(arrOfWrongCmd[i]));
+    }
+}
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_CmdParse);
+    return UNITY_END();
+}
